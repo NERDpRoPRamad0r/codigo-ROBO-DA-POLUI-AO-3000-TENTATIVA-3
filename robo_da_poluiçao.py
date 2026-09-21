78a1b02d7cf7ff028d385bd36a757a52862f9f0c
NERDpRoPRamad0r <graziele.balbo@gmail.com>
2026-09-21 18:56:09 -0300

    Refactor pollution robot game commands and logic

--- robo_da_poluiçao.py
+++ robo_da_poluiçao.py
@@ -1,35 +1,40 @@
+# bibliotecas
 import discord
 from discord.ext import commands
 import random
 import asyncio
 from discord.ext import commands, tasks
 
+# lista de ataques contra robo da poluição 3000
 lista_de_poluiçao_reciclar = [11, 12, 13, 15, 19, 23]
 lista_de_poluiçao_nao_gastar_agua = [62, 42, 35, 51, 53]
 lista_de_poluiçao_nao_gastar_energia = [54, 42, 49, 51, 53]
 lista_de_poluiçao_nao_usar_carro = [100, 102, 92, 12, 97]
 
+# lista da quantidade de poluição
 lista_de_poluiçao1 = [11, 12, 13, 15, 19, 23]
 lista_de_poluiçao2 = [62, 42, 35, 51, 53]
 lista_de_poluiçao3 = [54, 42, 49, 51, 53]
 lista_de_poluiçao4 = [100, 102, 92, 72, 97]
 
+# lista de vidas do robo da poluição 3000
 lista_de_vida_do_robo_da_poluiçao1 = [55, 64, 11, 57, 60, 59]
 lista_de_vida_do_robo_da_poluiçao2 = [62, 67, 58, 60, 71]
 lista_de_vida_do_robo_da_poluiçao3 = [123, 129, 112, 101, 95]
 lista_de_vida_do_robo_da_poluiçao4 = [11, 145, 172, 151, 97]
 
+# variaveis
 jogo_ativo = False
 poluiçao = 100
 vida = 1000
 
 
 
-# 1. Configuração de permissões
+# configuração de permissões
 intents = discord.Intents.default()
 intents.message_content = True
 
-# 2. Definindo o Bot (o nome aqui é bot_bozo)
+# definindo o bot
 robo_da_poluiçao = commands.Bot(command_prefix='/', intents=intents)
 
 @robo_da_poluiçao.event
@@ -38,6 +43,7 @@ async def on_ready():
     if not regeneracao_do_robo.is_running():
         regeneracao_do_robo.start()
 
+# se robo da poluição perder
 async def verificar_fim_jogo(ctx):
     global jogo_ativo, vida, poluiçao
     if vida <= 0:
@@ -49,6 +55,8 @@ async def verificar_fim_jogo(ctx):
 
 
         await asyncio.sleep(30) 
+       
+        # robo da poluição reiniciando se caso sua vida acabar
         
         jogo_ativo = False
         vida = 1000
@@ -56,19 +64,22 @@ async def verificar_fim_jogo(ctx):
         await ctx.send('EVENTO: 🔌 SISTEMA REINICIADO. O Robô da Poluição 3000 voltou a "funcionar" na arena!')
         if not regeneracao_do_robo.is_running():
             regeneracao_do_robo.start()
-    
+
+    # player perdendo
     if poluiçao >= 300:
         regeneracao_do_robo.stop() 
         await ctx.send('🎉 ParABÉns! VOcÊ pERDeu! A polUIçÃO subIU MUito! DESligaNdo')
 
         await asyncio.sleep(5) 
 
+        # robo reinicando se o player perdeu
         vida = 1000
         poluiçao = 100
         await ctx.send('EVENTO: 🔌 SISTEMA REINICIADO. O Robô da Poluição 3000 voltou a "funcionar" na arena!')
         if not regeneracao_do_robo.is_running():
             regeneracao_do_robo.start()
 
+# robo da poluição 3000 se curando
 @tasks.loop(seconds=5)
 async def regeneracao_do_robo():
     global vida, poluiçao, jogo_ativo
@@ -77,19 +88,21 @@ async def regeneracao_do_robo():
         vida += cura
         print(f"O robô se curou em {cura}! Vida atual: {vida}")
 
+# quando o player usa o comando de entrar na arena
 @robo_da_poluiçao.command()
 async def ir_na_arena_de_lixo(ctx):
     await ctx.send('--- BEM VINDO A ARENA DE LIXOS ---')
     await ctx.send('você foi na arena de lixo foi dificel de achar pois ela estava bem isolada, quando você chega, você ve a arena de lixo, ela era enorme para uma arena, e sim, toda feita de lixos como: latas,, pneus, sacos de lixo, lixos organicos, etc. Você entra na arena e quando entra, duas pilhas de pneus começam a pegar fogo como tochas, você ve nos seus lados tinha lixeiras de reciclagem, e você ve na sua frente um robô de dois metros de altura escrito no seu peito: anti-natureza 3000, ele parecia quebrado e descontinuado, parecendo que ele tinha sido abandonado la, e ele derrepende acorda e começa a dizer com uma voz robotica:')
     await ctx.send('olá-Humano,-eu-sou-o-MaiOr-inimigo-da-natureza-meu-mEStrE-fez-eu-pra-destruir-tudo-que-tem-de-natureza-nesse-mUNdo,-duvido-vc-me-derrotar-e-se-você-me-DeRRotar-humano-eu-vou-te-dar-uM-PreMIO(aceita a luta?)')
-   
+
+# se caso o player quiser usar o comando de enfrentar o robo mesmo nao estando na arena
 async def verificar_estado(ctx):
     global jogo_ativo, vida, poluiçao
     if not jogo_ativo:
         await ctx.send('⚠️: A luta não começou. Use `aceitar_luta_da_arena_de_lixo`.')
         return False
 
-
+# enfrentar o robo
 @robo_da_poluiçao.command()
 async def aceitar_luta_da_arena_de_lixo(ctx): 
     global jogo_ativo, vida, poluiçao
@@ -99,10 +112,11 @@ async def aceitar_luta_da_arena_de_lixo(ctx):
         await ctx.send(f'quantidade de poluiçao: {poluiçao}')
         jogo_ativo = True
         await verificar_estado(ctx)
+    # se a luta já começou
     else:
         await ctx.send('⚠️: A luta já começou!')
 
-
+# primeiro comando de ataque: RECICLAR
 @robo_da_poluiçao.command()
 async def reciclar(ctx):
     global jogo_ativo, vida, poluiçao
@@ -116,6 +130,7 @@ async def reciclar(ctx):
     
     return
 
+# segundo comando
 @robo_da_poluiçao.command()
 async def economizar_agua(ctx):
     global jogo_ativo, vida, poluiçao
